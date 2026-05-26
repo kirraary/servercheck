dbd40ae317e4bfa4384839c13722dd45fb7a0a11
kirraary <aldinoa055@gmail.com>
2026-05-26 09:46:06 +0700

    new favicon

--- auth/login.php
+++ auth/login.php
@@ -26,6 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ServerCheck Login</title>
+    <link rel="icon" type="image/png" href="assets/images/favicon.png">
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
